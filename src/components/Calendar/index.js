2d0335c71195ae5239ab39920212ea1e9c5be8fc
Alex Borges <alexborgessjc@hotmail.com>
2022-02-13 20:01:31 -0300

    Calendario obtendo os dados do Json

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { useState } from 'react';
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import React, { useEffect, useState } from 'react';
+//import { useState } from 'react';
+import axios from "axios";
+import {Inject, Day, Week, WorkWeek, Month, Agenda, ScheduleComponent} from '@syncfusion/ej2-react-schedule';
 import {CalendarDiv,TextDiv, ContentDiv, DateDiv, GraphDiv } from './styles';
 
 function CalendarSpace() { 
-    const [date, setDate] = useState(new Date());
+    //const [date, setDate] = useState(new Date());
 
-    const onChange = date =>{
-        setDate(date);
+    //const onChange = date =>{
+        //setDate(date);
+    //}    
+
+    const [evento, setEvento] = useState([]);
+
+    useEffect(() => {
+        async function getEvento() {
+        axios
+            .get(`http://localhost:3333/dataSource`)
+            .then((resp) => setEvento(resp.data))
+        }
+        getEvento();        
+    }, []);          
+     
+    const novoJson = [{
+                                     
+        StartTime: new Date(2022,1,15),
+        EndTime: new Date(2022,1,15)                                 
+    },
+    {
+                                    
+        StartTime: new Date(2022,1,16),
+        EndTime: new Date(2022,1,16)                                 
     }
+]  
 
-    console.log(date);
+    const novoJson2 = [];
 
+    evento.map((obj)=>{
+        novoJson2.push({"StartTime":new Date(obj.StartTime),"EndTime":new Date(obj.EndTime)})        
+    })
+    
+    console.log(novoJson2);
+    console.log(novoJson);
+                    
     return ( 
         <CalendarDiv>
             <TextDiv>Calendário</TextDiv>
             <ContentDiv>
-                <DateDiv><Calendar defaultView="month" onChange={onChange} value={date}/></DateDiv>
+                <DateDiv>
+                    <ScheduleComponent height='340px' currentView='Month' selectedDate={new Date()} eventSettings={{ dataSource: novoJson2 } }>
+                        <Inject services={[Day, Week, WorkWeek, Month, Agenda]}></Inject>
+                    </ScheduleComponent>
+                </DateDiv>
                 <GraphDiv>Gráfico</GraphDiv>
             </ContentDiv>
         </CalendarDiv>
